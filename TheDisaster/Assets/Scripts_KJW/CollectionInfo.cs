6c41a962d17c77d355b5033b0db9f96feb09ea04
JIU <94626295+ZoroZuro1@users.noreply.github.com>
2023-11-11 16:35:39 +0900

    collection scene setting

---
+++ TheDisaster/Assets/Scripts_KJW/CollectionInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionInfo : MonoBehaviour
+{
+    public Image infoImage;
+    public Text mediName;
+    public Text description;
+}
