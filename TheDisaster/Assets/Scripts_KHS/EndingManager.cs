dcc41ca10346de5d3d09c70fa4e70a80f713e08f
Hee Seo Kim <hisounni@ewhain.net>
2023-11-11 20:29:48 +0900

    khs

--- TheDisaster/Assets/Scripts_KHS/EndingManager.cs
+++ TheDisaster/Assets/Scripts_KHS/EndingManager.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class EndingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject panel;
+    [SerializeField] Text[] texts;
+
     void Start()
     {
-        
+        foreach (Text text in texts)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+
+        }
+
+        StartCoroutine(ShowTextsSequentially(4.0f, 1.0f));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ShowTextsSequentially(float textDisplayTime, float gapTime)
     {
-        
+        for (int i = 0; i < texts.Length; i++)
+        {
+            StartCoroutine(FadeTextIn(texts[i], 1.5f));
+
+            yield return new WaitForSeconds(textDisplayTime);
+
+            yield return new WaitForSeconds(gapTime);
+        }
+    }
+
+    IEnumerator FadeTextIn(Text text, float fadeInTime)
+    {
+        while (text.color.a < 1.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + (Time.deltaTime / fadeInTime));
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(fadeInTime);
+
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / fadeInTime));
+            yield return null;
+        }
     }
 }
